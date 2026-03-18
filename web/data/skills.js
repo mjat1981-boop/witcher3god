63341a82eb50b5ec7a5d090ad9c03c434288716e
mjat1981-boop <mjat1981@gmail.com>
2026-03-18 21:12:19 +0000

    Add hardcoded skills and mutations data

---
+++ web/data/skills.js
@@ -0,0 +1,32 @@
+const skills = {
+    combat: [
+        'Swordsmanship',
+        'Archery',
+        'Martial Arts'
+    ],
+    signs: [
+        'Igni',
+        'Aard',
+        'Quen',
+        'Axii',
+        'Yrden'
+    ],
+    alchemy: [
+        'Potion Brewing',
+        'Oil Preparation',
+        'Bomb Crafting'
+    ],
+    general: [
+        'Horse Riding',
+        'Navigation',
+        'Stealth'
+    ],
+    mutations: [
+        'Bloodbath',
+        'Euphoria',
+        'Metamorphosis',
+        'Furious Attack'
+    ]
+};
+
+module.exports = skills;
